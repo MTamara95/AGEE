f00befb3344f66d5a0479703e96ff36d2183d6d2
Tamara Marcetic <tamara.marcetic@avisto-eastern.com>
2023-08-09 16:35:20 +0200

    Revert "Implemented changes needed to try support ldapts through Angular"
    This reverts commit c86bf691c9fcf85b0fa04ecbf1710b2d68bc1ffb.

--- src/polyfill.ts
+++
@@ -1,3 +0,0 @@
-import * as process from 'process';
-
-window['process'] = process;
--- src/services/ldap.service.ts
+++ src/services/ldap.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Client, SearchResult } from 'ldapts';
+// import { Client, SearchResult } from 'ldapts';
 import {INVALID_CREDENTIALS_ERROR_CODE, INVALID_CREDENTIALS_ERROR_MESSAGE, ACTIVE_DIRECTORY_USERNAME} from '../consts/constants'
 import {EmployeeInterface} from '../models/employee.interface';
 
@@ -9,59 +9,62 @@ const domain = 'avisto-eastern.com';
   providedIn: 'root'
 })
 export class LdapService {
-  ldapClient: Client;
+  // ldapClient: Client;
 
-  constructor() { 
-    this.ldapClient = new Client({url: `ldap://${domain}`});
-  }
+  // constructor() { 
+  //   this.ldapClient = new Client({url: `ldap://${domain}`});
+  // }
 
-  async connect(username: string, password: string): Promise<void>{
-    try{
-      console.log('test1');
-      await this.ldapClient.bind(`${username}@${domain}`, password);
-      console.log(`test2:`);// ${this.ldapClient.isConnected}`);
-    }
-    catch(error){
-      console.log('greska :(' + error);
-      throw (error);
-    }
-  }
+  // async connect(username: string, password: string): Promise<void>{
+  //   try{
+  //     console.log(this.ldapClient);
+  //     await this.ldapClient.bind(`${username}@${domain}`, password);
+  //     console.log(`test2: ${this.ldapClient.isConnected}`);
+  //   }
+  //   catch(error){
+  //     console.log('greska :(');
+  //     throw (error);
+  //   }
+  // }
 
   async search(username: string, password: string): Promise<EmployeeInterface>{
-    try{
-      await this.connect(username, password);
+    const employee: EmployeeInterface = {'emailAddress': 'test email', 'role': 'test role'} as EmployeeInterface;
 
-      const searchResult = await this.ldapClient.search(
-        domain.split('.').map(e => `dc=${e}`).join(','),
-          {
-            filter: `(${ACTIVE_DIRECTORY_USERNAME}=${username}@${domain})`
-          }
-      );
+    return employee;
+    // try{
+    //   await this.connect(username, password);
 
-      const emailAddress = searchResult.searchEntries[0]['mail'];
-      const role = searchResult.searchEntries[0]['role'];
+    //   const searchResult = await this.ldapClient.search(
+    //     domain.split('.').map(e => `dc=${e}`).join(','),
+    //       {
+    //         filter: `(${ACTIVE_DIRECTORY_USERNAME}=${username}@${domain})`
+    //       }
+    //   );
 
-      const employee: EmployeeInterface = {'emailAddress': emailAddress, 'role': role} as EmployeeInterface;
+    //   const emailAddress = searchResult.searchEntries[0]['mail'];
+    //   const role = searchResult.searchEntries[0]['role'];
 
-      return employee;
-    }
-    catch(error){
-      throw(error);
-    }
-    finally{
-      this.ldapClient.unbind();
-    }
+    //   const employee: EmployeeInterface = {'emailAddress': emailAddress, 'role': role} as EmployeeInterface;
+
+    //   return employee;
+    // }
+    // catch(error){
+    //   throw(error);
+    // }
+    // finally{
+    //   this.ldapClient.unbind();
+    // }
   }
 
-  stringConvert(entry: SearchResult): string{
-    if(typeof entry === 'string'){
-      return entry;
-    }
-    else if(Array.isArray(entry)){ // obraditi niz buffera, niz string-ova...
-      return entry.join(' ');
-    }
-    else{
-      throw(`nepoznat tip:${typeof entry}`);
-    }
-  }
+  // stringConvert(entry: SearchResult): string{
+  //   if(typeof entry === 'string'){
+  //     return entry;
+  //   }
+  //   else if(Array.isArray(entry)){ // obraditi niz buffera, niz string-ova...
+  //     return entry.join(' ');
+  //   }
+  //   else{
+  //     throw(`nepoznat tip:${typeof entry}`);
+  //   }
+  // }
 }
