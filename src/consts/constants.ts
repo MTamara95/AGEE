91b6f5ae10e05d7d5cfc0a95daa9d888a6cdcea0
Tamara Marcetic <tamara.marcetic@avisto-eastern.com>
2023-08-09 10:28:48 +0200

    Edited logic not to use separated ldap service; currently hardcoded auth case, due to the problems with setting up ldap auth logic in an angular app

--- src/consts/constants.ts
+++ src/consts/constants.ts
@@ -1,2 +1,5 @@
 export const API_URL = "http://localhost:3000/api/ldap/search";
 export const UNSUCCESSFUL_AUTHENTICATION_ERROR_MESSAGE = "An error occured during the authentication. Please try again";
+export const INVALID_CREDENTIALS_ERROR_CODE = 49;
+export const INVALID_CREDENTIALS_ERROR_MESSAGE = 'Invalid credentials';
+export const ACTIVE_DIRECTORY_USERNAME = 'userPrincipalName';
