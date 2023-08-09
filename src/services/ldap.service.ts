9e12357a5a84d5049f61dd19adba2d088c4b604a
Tamara Marcetic <tamara.marcetic@avisto-eastern.com>
2023-08-09 16:35:36 +0200

    Revert "Edited logic not to use separated ldap service; currently hardcoded auth case, due to the problems with setting up ldap auth logic in an angular app"
    This reverts commit 91b6f5ae10e05d7d5cfc0a95daa9d888a6cdcea0.

--- src/services/ldap.service.ts
+++ src/services/ldap.service.ts
@@ -1,70 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-// import { Client, SearchResult } from 'ldapts';
-import {INVALID_CREDENTIALS_ERROR_CODE, INVALID_CREDENTIALS_ERROR_MESSAGE, ACTIVE_DIRECTORY_USERNAME} from '../consts/constants'
-import {EmployeeInterface} from '../models/employee.interface';
-
-const domain = 'avisto-eastern.com';
+import { API_URL } from 'src/consts/constants';
+import { EmployeeInterface } from 'src/models/employee.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LdapService {
-  // ldapClient: Client;
-
-  // constructor() { 
-  //   this.ldapClient = new Client({url: `ldap://${domain}`});
-  // }
-
-  // async connect(username: string, password: string): Promise<void>{
-  //   try{
-  //     console.log(this.ldapClient);
-  //     await this.ldapClient.bind(`${username}@${domain}`, password);
-  //     console.log(`test2: ${this.ldapClient.isConnected}`);
-  //   }
-  //   catch(error){
-  //     console.log('greska :(');
-  //     throw (error);
-  //   }
-  // }
-
-  async search(username: string, password: string): Promise<EmployeeInterface>{
-    const employee: EmployeeInterface = {'emailAddress': 'test email', 'role': 'test role'} as EmployeeInterface;
-
-    return employee;
-    // try{
-    //   await this.connect(username, password);
-
-    //   const searchResult = await this.ldapClient.search(
-    //     domain.split('.').map(e => `dc=${e}`).join(','),
-    //       {
-    //         filter: `(${ACTIVE_DIRECTORY_USERNAME}=${username}@${domain})`
-    //       }
-    //   );
-
-    //   const emailAddress = searchResult.searchEntries[0]['mail'];
-    //   const role = searchResult.searchEntries[0]['role'];
-
-    //   const employee: EmployeeInterface = {'emailAddress': emailAddress, 'role': role} as EmployeeInterface;
-
-    //   return employee;
-    // }
-    // catch(error){
-    //   throw(error);
-    // }
-    // finally{
-    //   this.ldapClient.unbind();
-    // }
+  constructor(private http: HttpClient) { 
   }
 
-  // stringConvert(entry: SearchResult): string{
-  //   if(typeof entry === 'string'){
-  //     return entry;
-  //   }
-  //   else if(Array.isArray(entry)){ // obraditi niz buffera, niz string-ova...
-  //     return entry.join(' ');
-  //   }
-  //   else{
-  //     throw(`nepoznat tip:${typeof entry}`);
-  //   }
-  // }
+  async search(username: string, password: string){
+    const body = {username, password};
+    try{
+      const employee: Promise<EmployeeInterface> = await this.http.post<any>(API_URL, body).toPromise();
+      return employee;
+    }
+    catch(error){
+      console.log(error);
+      return null;
+    }
+  }
 }
