710439dd4925220b5ae8c677a145af0389542769
Tamara Marcetic <tamara.marcetic@avisto-eastern.com>
2023-08-14 13:11:33 +0200

    Fixed issues with some tests after reverting

--- src/services/ldap.service.spec.ts
+++ src/services/ldap.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LdapService } from './ldap.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LdapService', () => {
   let service: LdapService;
+  let search: jasmine.Spy<jasmine.Func> | ((arg0: string, arg1: string) => void);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(LdapService);
+    search = jasmine.createSpy('search');
+    search('test username', 'test password');
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('tracks that the search was called', () => {
+    expect(search).toHaveBeenCalled();
   });
 });
