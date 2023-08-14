388190e28ef5367f5b6d53c3eacc4f61016faaa4
Tamara Marcetic <tamara.marcetic@avisto-eastern.com>
2023-08-14 12:17:02 +0200

    Revert "Added a test for ldap service (async search metod, creating spy)"
    This reverts commit fe75160e8b753f64cd8799ec475cc08612a094a7.

--- src/services/ldap.service.spec.ts
+++ src/services/ldap.service.spec.ts
@@ -5,24 +5,16 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LdapService', () => {
   let service: LdapService;
-  let search: jasmine.Spy<jasmine.Func> | ((arg0: string, arg1: string) => void);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
     });
-
     service = TestBed.inject(LdapService);
-    search = jasmine.createSpy('search');
-    search('test username', 'test password');
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-
-  it('tracks that the search was called', () => {
-    expect(search).toHaveBeenCalled();
   });
 
 });
